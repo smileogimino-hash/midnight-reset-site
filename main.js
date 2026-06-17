ca12647128e5af2d39faf69bd2d7d9d42a7064e8
Pocket <smile.ogi.mino@gmail.com>
2026-06-17 17:07:07 +0900

    add:カーテンの画像を追加

--- main.js
+++ main.js
@@ -46,3 +46,71 @@ toggle.addEventListener("change", () => {
     screenDark.classList.remove("is-active");
   }
 });
+
+/* =========================
+   カーテン
+========================= */
+const message = document.querySelector(".message");
+
+const screenDark = document.querySelector(".screen-dark");
+
+const curtainOpen = document.querySelectorAll(".curtain-open");
+const curtainClosing = document.querySelectorAll(".curtain-closing");
+const curtainClosed = document.querySelector(".curtain-closed");
+
+let hasStartedCurtain = false;
+
+/* =========================
+   カーテン開始
+========================= */
+
+function startCurtainSequence() {
+  if (hasStartedCurtain) return;
+
+  hasStartedCurtain = true;
+
+  /* opened表示 */
+  curtainOpen.forEach((img) => {
+    img.classList.add("is-show");
+  });
+
+  /* 2秒後 */
+  setTimeout(() => {
+    curtainOpen.forEach((img) => {
+      img.classList.remove("is-show");
+    });
+
+    curtainClosing.forEach((img) => {
+      img.classList.add("is-show");
+    });
+
+    /* 暗転開始 */
+    screenDark.classList.add("is-active");
+  }, 2000);
+
+  /* さらに2秒後 */
+  setTimeout(() => {
+    curtainClosing.forEach((img) => {
+      img.classList.remove("is-show");
+    });
+
+    curtainClosed.classList.add("is-show");
+  }, 4000);
+}
+
+/* =========================
+   message監視
+========================= */
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      /* 数秒待つ */
+      setTimeout(() => {
+        startCurtainSequence();
+      }, 3000);
+    }
+  });
+});
+
+observer.observe(message);
